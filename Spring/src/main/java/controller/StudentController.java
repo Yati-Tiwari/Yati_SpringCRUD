d52e55d44ec36ec87df6c038e29e91d0b8288812
Yati Tiwari <tiwariyati221@gmail.com>
2026-04-29 15:18:26 +0530

    Initial Spring Boot CRUD project

---
+++ Spring/src/main/java/controller/StudentController.java
@@ -0,0 +1,46 @@
+package com.example.Spring.controller;
+
+import com.example.Spring.model.Student;
+import com.example.Spring.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public String createStudent(@RequestBody Student student) {
+        service.createStudent(student);
+        return "Student created";
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudent(@PathVariable int id) {
+        return service.getStudentById(id);
+    }
+
+    @PutMapping("/{id}")
+    public String updateStudent(@PathVariable int id, @RequestBody Student student) {
+        service.updateStudent(id, student);
+        return "Student updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteStudent(@PathVariable int id) {
+        service.deleteStudent(id);
+        return "Student deleted";
+    }
+}
